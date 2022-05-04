442f7bcdce4f3323012347d02b4c5dff74c45d80
Ram <enespra@gmail.com>
2022-05-04 21:55:23 +0800

    [Misc] New folder structure (#2)
    * change the function name of the route function
    
    * revert
    
    * added v1 folder
    
    * remove __pycache__
    
    * added .gitignore
    
    * delete models/dog.py
    
    * updated app.py
    
    [Feature] Added cats endpoints and db
    
    [Misc] Added Cats to new folder structure
    
    * [Feature] Added cats endpoints and db
    
    * [Misc] Updated app.py
    
    Co-authored-by: Arthur Layese <arthur@layese.com>

--- server/src/app.py
+++ server/src/app.py
@@ -1,6 +1,7 @@
 import json
 from flask import Flask, request, jsonify, g
 from v1.dog.router import DogRouter
+from v1.cat.router import CatRouter
 from v1.auth import login as auth_login, verify_token as auth_verify_token
 from flask_cors import CORS
 
@@ -15,6 +16,7 @@ CORS(app, resources={r"*": {"origins": [
 ]}},  supports_credentials=True)
 
 app.register_blueprint(DogRouter.handler())
+app.register_blueprint(CatRouter.handler())
 
 @app.route('/v1/login', methods=['POST'])
 def login():
@@ -35,42 +37,6 @@ def verify_token():
     if not auth_verify_token(token):
         return jsonify({'message': 'Invalid token'}), 403
     return jsonify({'ok': 'Token is valid'})
-
-@app.route('/cats', methods=['POST', 'GET', 'PUT', 'DELETE'])
-def cats():
-    cat = Cat()
-    if str(request.method).upper() == 'POST':
-        resp = cat.post(request.json)
-        if resp == False:
-            return make_response(jsonify({
-                "error": "Failed to add. There are items in your request that are invalid."
-            }), 400)
-        return jsonify(resp)
-    if str(request.method).upper() == 'PUT':
-        return jsonify(cat.put(request.json))
-    if str(request.method).upper() == 'DELETE':
-        return jsonify(cat.delete(request.json))
-    cats = cat.get()
-    return jsonify(cats)
-
-@app.route('/cats/<cat_id>', methods=['GET', 'PUT', 'DELETE'])
-def cat(cat_id):
-    cat_object = Cat()
-    cat = cat_object.get({"id": cat_id})
-    if cat is None:
-        return make_response(jsonify({"error": "Cat id not found."}), 404)
-    if str(request.method).upper() == 'PUT':
-        cat_data = request.json
-        cat_data['id'] = cat_id
-        resp = cat_object.put(cat_data)
-        if resp == False:
-            return make_response(jsonify({
-                "error": "Failed to update. There are items in your request that are invalid."
-            }), 400)
-        return jsonify(resp)
-    if str(request.method).upper() == 'DELETE':
-        return jsonify(cat_object.delete(cat_id))
-    return jsonify(cat)
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=8080)
